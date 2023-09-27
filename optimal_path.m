34f0548fc079ffa5c88b9cc39ea350d9e5234de1
Michele Focchi <michele.focchi@iit.it>
2023-09-27 19:01:40 +0200

    converges with 3 modelts with dubin init! for difficult target!

--- optimal_path.m
+++ optimal_path.m
@@ -18,15 +18,15 @@ p0 = [0.0; 0.0; -0.3];
 %pf = [1; 1.0; -1.9];%pf = [10.0; 10.0; -1.9]; % works with UNICYCLE TODO
 %%does not converge with LONGSLIP
 %pf = [1; -2.0; -1.9];%pf = [10.0; 10.0; -1.9];
-pf = [1.5; 1.0; -1.2];%pf = [10.0; 10.0; -1.9]; % works with LONGSLIP
+pf = [1.; 1.0; -1.9];%pf = [10.0; 10.0; -1.9]; % works with LONGSLIP
 
 
 params.int_method = 'rk4'; %'eul'  'rk4';
 params.N_dyn = 40; %dynamic constraints (number of knowts in the discretization) 
-params.int_steps = 15 ;%cast(5,"int64"); %0 means normal intergation
+params.int_steps = 10 ;%cast(5,"int64"); %0 means normal intergation
 params.num_params = 1; %final time
 
-params.w1= 0;  % minimum time 
+params.w1= 1;  % minimum time (fundamental!)
 params.w2= 1; % smoothing   1000
 params.w3= 0; %soft tracking of end target (is already in the constraints not needed)
 params.w4= 0; % invariant set TODO
@@ -54,95 +54,24 @@ params.slip_fit_coeff.right = [0.0390,    0.2499 ];
 params.side_slip_fit_coeff = [-0.7758   -6.5765];
 params.slip_fit_coeff.min_value = -1000;
 
+% normal init  (no longer used)
+% t0 = norm(pf(1:2) - p0(1:2))/(0.5*params.v_max); 
+% omega_l0 = 0.5*params.omega_w_max*ones(1,params.N_dyn); %TODO gives issue with 0, should be initialized with half
+% omega_r0 = 0.5*params.omega_w_max*ones(1,params.N_dyn);
+% %  
 
-t0 = norm(pf(1:2) - p0(1:2))/(0.5*params.v_max); 
-omega_l0 = 0.5*params.omega_w_max*ones(1,params.N_dyn); %TODO gives issue with 0, should be initialized with half
-omega_r0 = 0.5*params.omega_w_max*ones(1,params.N_dyn);
- 
-
-% % do init with dubins
-% dubinsObj= dubinsClass;
-% % Find optimal Dubins solution
-% [pidx, curve, k, lengths] = dubinsObj.dubins_shortest_path(p0(1), p0(2), p0(3), pf(1), pf(2), pf(3), params.omega_max);
-% 
-% % build up velocity trajectories
-% sample_instants = floor(lengths/sum(lengths)*params.N_dyn);
-% omega0 = 0.*ones(1,params.N_dyn);
-% v0 = params.v_max.*ones(1,params.N_dyn);
-% idx1 = sample_instants(1);
-% idx2 = sample_instants(1)+sample_instants(2);
-% idx3 = sum(sample_instants);
-% omega0(1:idx1) = k(1);
-% omega0(idx1+1:idx2) = k(2);
-% omega0(idx2+1:idx3) = k(3);
-% r = params.sprocket_radius;
-% B = params.width;
-% A = [r/2, r/2; r/B, -r/B];
-
-% for i=1:params.N_dyn
-%   y = inv(A)*[v0(i); omega0(i)];
-%   omega_r0(i) = params.gearbox_ratio*y(1); 
-%   omega_l0(i) = params.gearbox_ratio* y(2);
-% end
-
-%     dt = 0.001;
-%     % resample inputs 
-%     n_samples = floor(t0/dt);
-%     omega_l_fine = zeros(1,n_samples);
-%     omega_r_fine = zeros(1,n_samples);
-%     rough_count = 1;
-%     t_ = 0;
-%     for i=1: n_samples
-%        t_= t_+dt;
-%        if t_>= ((n_samples) *dt/(params.N_dyn-1))
-%             rough_count = rough_count + 1;
-%             t_ =0;
-%         end  
-%         omega_l_fine(i) =  omega_l0(rough_count);
-%         omega_r_fine(i) =  omega_r0(rough_count);
-%     end
-% 
-%     params.int_steps = 0;
-%     [states, t] = computeRollout(p0, 0,dt, n_samples, omega_l_fine, omega_r_fine, params);
-%     
-%     x = states(1,:);
-%     y = states(2,:);
-%     theta = states(3,:);
-% 
-% p =  [x; y; theta];
-%   figure
-% plot(pf(1), pf(2) , 'Marker', '.', 'Color','r', 'MarkerSize',60) ;
-% min_x = min(min(p(1,:)), pf(1))-3 ;
-% max_x = max(max(p(1,:)), pf(1))+3 ;
-% min_y = min(min(p(2,:)), pf(2))-3 ;
-% max_y = max(max(p(2,:)), pf(2)) +3 ;
-% 
-% % set(gca,'XLim',[min_x max_x])
-% % set(gca,'YLim',[min_y max_y])
-% 
-% color_input = 'r'; 
-% 
-% % actual traj
-% plot(p(1,:), p(2,:), 'Color', color_input ) ;
-% 
-% 
-% scaling = 0.1*norm(pf(1:2) - p0(1:2));
-% %initial orient
-% plotOrientation([p0(1); p0(2)], p0(3), scaling);
-% %final orient 
-% plotOrientation([pf(1); pf(2)], pf(3), 0.1);
-% grid on;
-% 
-% xlabel('X');
-% ylabel('Y');
-
-%with matlab
-% dubConnObj = dubinsConnection;
-% dubConnObj.MinTurningRadius = 1/params.omega_max;
-% [pathSegObj, pathCosts] = connect(dubConnObj,p0',pf');
-% show(pathSegObj{1})
-
-
+% do init with dubins
+dubConnObj = dubinsConnection
+curvature_max = params.omega_max/params.v_max;
+dubConnObj.MinTurningRadius = 1/curvature_max;
+[pathSegObj, pathCosts] = connect(dubConnObj,p0',pf');
+show(pathSegObj{1})
+% get total time
+t0 = sum(pathSegObj{1}.MotionLengths)/params.v_max;
+% compute the omega from dubin
+omegas = get_omega_from_dubins(pathSegObj{1}, params.v_max, 1/curvature_max);
+%map to wheel omega
+[omega_l0, omega_r0, t_rough] = getVelocityParamsFromDubin(params, pathSegObj{1}.MotionLengths, omegas);
 
 if GENCODE
     %generates the cpp code
