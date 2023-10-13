2a5d4e3b34a23df77e2341756e8a2cebc424ff6d
DavDori <47893850+DavDori@users.noreply.github.com>
2023-10-13 13:38:14 +0200

    remove gearbox ratio parameter

--- getVelocityParamsFromDubin.m
+++ getVelocityParamsFromDubin.m
@@ -27,8 +27,8 @@ function [omega_l0, omega_r0, time] = getVelocityParamsFromDubin(params, lengths
         B = params.width;
         A = [r/2, r/2; r/B, -r/B];
         y = inv(A)*[params.v_max; omega];
-        omega_r0 = [omega_r0 params.gearbox_ratio*y(1)]; 
-        omega_l0 = [omega_l0 params.gearbox_ratio* y(2)];
+        omega_r0 = [omega_r0 y(1)]; 
+        omega_l0 = [omega_l0 y(2)];
         time = [time t_ ];
         t_ =  t_+ dt;
     end
