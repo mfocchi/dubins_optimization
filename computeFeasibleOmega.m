61e7d5f99323daf6577b745d759a57cbf7af4129
Michele Focchi <michele.focchi@unitn.it>
2024-06-12 18:28:04 +0200

    bugfix harcoded v_nom!

--- computeFeasibleOmega.m
+++ computeFeasibleOmega.m
@@ -1,14 +1,14 @@
-function [omega_min, omega_max] = computeFeasibleOmega(v)
+function [omega_min, omega_max] = computeFeasibleOmega(long_v)
     constants.GEARBOX = 34.45;
     constants.TRACK_WIDTH = 0.606;
     constants.SPROCKET_RADIUS = 0.0856;
     constants.WHEEL_MAX = 160;
     
-    v_nom = 0.2;
+
     
     C = [-constants.SPROCKET_RADIUS/(constants.TRACK_WIDTH*constants.GEARBOX); constants.SPROCKET_RADIUS/(constants.TRACK_WIDTH*constants.GEARBOX)];
     Aeq = [constants.SPROCKET_RADIUS/(2*constants.GEARBOX ), constants.SPROCKET_RADIUS/(2*constants.GEARBOX )];
-    beq = v_nom;
+    beq = long_v;
     lb = [-constants.WHEEL_MAX, -constants.WHEEL_MAX];
     ub = [constants.WHEEL_MAX, constants.WHEEL_MAX];
     [x, feval] = linprog(C,[],[],Aeq,beq,lb,ub);
